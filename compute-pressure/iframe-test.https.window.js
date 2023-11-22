86696949b84a216828eff8ec65149783f1899af3
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2023-11-22 12:00:30 +0100

    Focus the iframe, add some message passing
    - Copy some of the generic sensors message passing infrastructure to
      properly communicate with the iframe.
    - Explicitly focus the iframe, otherwise it will not send any updates due to
      failing privacy checks.

--- compute-pressure/iframe-test.https.window.js
+++ compute-pressure/iframe-test.https.window.js
@@ -3,13 +3,42 @@
 
 'use strict';
 
-promise_test(t => {
+function send_message_to_iframe(iframe, message) {
+  return new Promise((resolve, reject) => {
+    window.addEventListener('message', (e) => {
+      // The usage of test_driver.set_test_context() in
+      // the iframe causes unrelated messages to be sent as
+      // well. We just need to ignore them here.
+      if (!e.data.command) {
+        return;
+      }
+
+      if (e.data.command !== message.command) {
+        reject(`Expected reply with command '${message.command}', got '${
+            e.data.command}' instead`);
+        return;
+      }
+      if (e.data.error) {
+        reject(e.data.error);
+        return;
+      }
+      resolve(e.data.result);
+    });
+    iframe.contentWindow.postMessage(message, '*');
+  });
+}
+
+promise_test(async t => {
     let frame = document.createElement('iframe');
     frame.allow = "compute-pressure";
     frame.src = get_host_info().HTTPS_REMOTE_ORIGIN + "/compute-pressure/foo.https.html";
+    const eventWatcher = new EventWatcher(t, frame, 'load');
     document.body.appendChild(frame);
+    t.add_cleanup(() => { frame.parentNode.removeChild(frame) });
+    await eventWatcher.wait_for('load');
 
     assert_not_equals(frame.src, location.href);
 
-    return new Promise(r => { t.step_timeout(r, 1000) });
+    frame.contentWindow.focus();
+    await send_message_to_iframe(frame, {command: 'start'});
 }, 'foo');
